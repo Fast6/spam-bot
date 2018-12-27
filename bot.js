28c01490c0a6fba5826e3d2d1a076ebe8bd7ca42
Fast6 <45312443+Fast6@users.noreply.github.com>
2018-12-27 20:03:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const client2 = new Discord.Client();
 
 client.on('ready', async() => {
 var server = "411555917482426368"; // ايدي السررفر
@@ -11,3 +11,10 @@ var channel = "411568552722694155";//ايدي الروم
 })
 
 client.login(process.env.BOT_TOKEN);
+client2.on('ready', async() => {
+var server = "411555917482426368"; // ايدي السررفر
+var channel = "411570017818116096";//ايدي الروم
+    setInterval(()=>{
+    client2.guilds.get(server).channels.get(channel).send('** Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz**')
+    },305);
+    })
