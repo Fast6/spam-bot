607d58760524416b6dd6cb12a57d16ab46703c38
Fast6 <45312443+Fast6@users.noreply.github.com>
2018-12-28 12:46:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
+const client5 = new Discord.Client();
 
 client.on('ready', async() => {
 var server = "411555917482426368"; // ايدي السررفر
@@ -34,8 +35,15 @@ var channel = "411579989918482434";//ايدي الروم
     client4.guilds.get(server).channels.get(channel).send('** Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast Fast**')
     },305);
 })
+client5.on('ready', async() => {
+var server = "515229161804267520"; // ايدي السررفر
+var channel = "515230042536804353";//ايدي الروم
+    setInterval(()=>{
+    client5.guilds.get(server).channels.get(channel).send('** Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz Azoz**')
+    },305);
 
 client.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN2);
 client3.login(process.env.BOT_TOKEN3);
 client4.login(process.env.BOT_TOKEN4);
+client5.login(process.env.BOT_TOKEN5);
